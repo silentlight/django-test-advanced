90617c946f89a8042b515871c40e0ad9ebeb3414
Bartosz Hejman <bartoszhejman@gmail.com>
2019-09-01 13:46:31 +0200

    Add create recipe endpoint

--- recipe/views.py
+++ recipe/views.py
@@ -53,3 +53,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return self.serializer_class
 
+    def perform_create(self, serializer):
+        """Create a new object"""
+        serializer.save(user=self.request.user)
+
